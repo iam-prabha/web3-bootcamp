8bbfa8844b848cc3e8284dca80c6779253d75fa4
Velprabhakaran Baskar <100754859+iam-prabha@users.noreply.github.com>
2024-10-24 14:24:34 +0530

    Add files via upload

---
+++ favorite/src/lib.rs
@@ -0,0 +1,62 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6aNKZ7feYfkijwjZy2r9p6AsSsJ4CUVSiZhg2oYoopzY");
+//  how many account need a space for each account
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites( 
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+
+        ) -> Result<()> {
+        msg!("Greeting from {}",context.program_id);
+        let user_public_key = context.accounts.user.key();
+
+                                                                                                         // :? expands           
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is {color}, and their hobbies:?");
+
+        context.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+
+        // rust return response 
+        Ok(())
+    }
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites{
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5,50)]
+    pub hobbies: Vec<String>,
+}
+
+#[derive(Accounts)]
+pub struct  SetFavorites<'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump // calculate seed that we given
+    )]
+    pub favorites: Account<'info,Favorites>,
+
+    pub system_program: Program<'info,System>,
+
+}
